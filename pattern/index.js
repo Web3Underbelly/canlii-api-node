d483d35a3416438e1a7c325fcc1de3eb7c79024d
William Barnes <william@webarnes.ca>
2014-05-19 21:04:14 -0400

    Deleted directory created by blanket.

--- pattern/index.js
+++
@@ -1,6 +0,0 @@
-if (typeof _$jscoverage === 'undefined') _$jscoverage = {};
-if (typeof _$jscoverage['lib/index.js'] === 'undefined'){_$jscoverage['lib/index.js']=[];
-_$jscoverage['lib/index.js'].source=['module.exports = 123;'];
-_$jscoverage['lib/index.js'][1]=0;
-}_$jscoverage['lib/index.js'][1]++;
-module.exports = 123;
